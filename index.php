a2b5c306713a4820ce7ed7f64f63249029a10350
Lou-Anne <saejinmx@gmail.com>
2024-04-23 13:50:08 +0200

    Première section

--- index.php
+++ index.php
@@ -6,10 +6,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Astra</title>
-    <link rel="stylesheet" href="css/main.scss" />
+    <link rel="stylesheet" href="css/main.css" />
 </head>
 
 <body>
+    <header>
+        <p class="logo">Astra</p>
+        <nav>
+            <a href="">Explorer les planètes</a>
+            <a href="">Créer une planète</a>
+            <a href="">Log In</a>
+            <a href="" class="bu-head">Register</a>
+        </nav>
+    </header>
+
+    <section class="planet">
+        <img src="/src/Planet1.png" alt="">
+        <p>Concevez, et donnez vie à vos propres planètes !</p>
+    </section>
+
     
 </body>
 
