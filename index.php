f58502545fd3f28e42e1664721afafd0e0fc0bf2
Lou-Anne <saejinmx@gmail.com>
2024-04-23 11:36:16 +0200

    config scss et variables

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Astra</title>
+    <link rel="stylesheet" href="css/main.scss" />
+</head>
+
+<body>
+    
+</body>
+
+</html>
