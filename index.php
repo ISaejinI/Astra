0278614abe365bac2124a11a2a37e7b2f73865bd
Lou-Anne <saejinmx@gmail.com>
2024-04-23 14:34:19 +0200

    Carroussel cassé

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Astra</title>
-    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/main.css"/>
     <script src="/script/script.js"></script>
 </head>
 
@@ -32,24 +32,24 @@
             <div class="swiper">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide">
-                        <img src="" alt="">
-                        <p></p>
+                        <img src="/src/Planet1.png" alt="">
+                        <p>Mercure</p>
                     </div>
                     <div class="swiper-slide">
-                        <img src="" alt="">
-                        <p></p>
+                        <img src="/src/Planet2.png" alt="">
+                        <p>Venus</p>
                     </div>
                     <div class="swiper-slide">
-                        <img src="" alt="">
-                        <p></p>
+                        <img src="/src/Planet3.png" alt="">
+                        <p>Terre</p>
                     </div>
                     <div class="swiper-slide">
-                        <img src="" alt="">
-                        <p></p>
+                        <img src="/src/Planet4.png" alt="">
+                        <p>Lune</p>
                     </div>
                     <div class="swiper-slide">
-                        <img src="" alt="">
-                        <p></p>
+                        <img src="/src/Planet5.png" alt="">
+                        <p>Pluton</p>
                     </div>
                 </div>
             </div>
