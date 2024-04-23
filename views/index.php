550f451475ef120e643a8790df41d88fff957201
Lou-Anne <saejinmx@gmail.com>
2024-04-23 15:30:27 +0200

    restructuration fichier

--- views/index.php
+++ views/index.php
@@ -1,56 +1,38 @@
-<?php  ?>
-<!DOCTYPE html>
-<html lang="fr">
+<?php require "../template/header.html" ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Astra</title>
-    <link rel="stylesheet" href="../css/main.css"/>
-    <link rel="stylesheet" href="../node_modules/swiper/swiper.css">
-    <link rel="stylesheet" href="../node_modules/swiper/modules/navigation.css">
-    <script type="module" src="/script/script.js"></script>
-</head>
+<section id="planet">
+    <img src="../src/Planet1.png" alt="">
+    <p>Concevez, et donnez vie à vos propres planètes !</p>
+</section>
 
-<body>
-    <?php require "../template/header.html" ?>
-
-    <section id="planet">
-        <img src="../src/Planet1.png" alt="">
-        <p>Concevez, et donnez vie à vos propres planètes !</p>
-    </section>
-
-    <section id="carousel">
-        <div class="box">
-            <h1>Les dernières planètes découvertes</h1>
-            <div class="swiper">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <img src="../src/Planet1.png" alt="">
-                        <p>Mercure</p>
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="../src/Planet2.png" alt="">
-                        <p>Venus</p>
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="../src/Planet3.png" alt="">
-                        <p>Terre</p>
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="../src/Planet4.png" alt="">
-                        <p>Lune</p>
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="../src/Planet5.png" alt="">
-                        <p>Pluton</p>
-                    </div>
+<section id="carousel">
+    <div class="box">
+        <h1>Les dernières planètes découvertes</h1>
+        <div class="swiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="../src/Planet1.png" alt="">
+                    <p>Mercure</p>
+                </div>
+                <div class="swiper-slide">
+                    <img src="../src/Planet2.png" alt="">
+                    <p>Venus</p>
+                </div>
+                <div class="swiper-slide">
+                    <img src="../src/Planet3.png" alt="">
+                    <p>Terre</p>
+                </div>
+                <div class="swiper-slide">
+                    <img src="../src/Planet4.png" alt="">
+                    <p>Lune</p>
+                </div>
+                <div class="swiper-slide">
+                    <img src="../src/Planet5.png" alt="">
+                    <p>Pluton</p>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <?php require "../template/footer.html" ?>
-</body>
-
-</html>
+<?php require "../template/footer.html" ?>
