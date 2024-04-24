415e73c751c69fb2d01d81346a4305a175d7afce
Lou-Anne <saejinmx@gmail.com>
2024-04-24 12:05:45 +0200

    Début mise en page des planètes

--- views/planets.php
+++ views/planets.php
@@ -3,7 +3,48 @@
 <section id="all-planets">
     <div id="filter">Mes éléments</div>
     <div id="planets">
-        
+        <div class="one-planet">
+            <a href="./planet.php">
+                <img src="../src/Planet3.png" alt="">
+                <p>Pluton</p>
+            </a>
+        </div>
+        <div class="one-planet">
+            <a href="./planet.php">
+                <img src="../src/Planet3.png" alt="">
+                <p>Pluton</p>
+            </a>
+        </div>
+        <div class="one-planet">
+            <a href="./planet.php">
+                <img src="../src/Planet3.png" alt="">
+                <p>Pluton</p>
+            </a>
+        </div>
+        <div class="one-planet">
+            <a href="./planet.php">
+                <img src="../src/Planet3.png" alt="">
+                <p>Pluton</p>
+            </a>
+        </div>
+        <div class="one-planet">
+            <a href="./planet.php">
+                <img src="../src/Planet3.png" alt="">
+                <p>Pluton</p>
+            </a>
+        </div>
+        <div class="one-planet">
+            <a href="./planet.php">
+                <img src="../src/Planet3.png" alt="">
+                <p>Pluton</p>
+            </a>
+        </div>
+        <div class="one-planet">
+            <a href="./planet.php">
+                <img src="../src/Planet3.png" alt="">
+                <p>Pluton</p>
+            </a>
+        </div>
     </div>
 </section>
 
