afd311ffd21cbe168fb80f13f580f9b63f328181
Lou-Anne <saejinmx@gmail.com>
2024-04-26 15:07:44 +0200

    Affichege des informations de la planète

--- views/planet.php
+++ views/planet.php
@@ -1,6 +1,11 @@
 <?php 
 
+$req = 'SELECT * FROM planets WHERE id=?';
 
+$planet = $dbAstra->prepare($req);
+$planet->execute(array($_GET['id']));
+$line = $planet->fetch();
 
+var_dump($line);
 
 ?>
