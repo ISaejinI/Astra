14d2344c2082758cd7aad82fab57bbeed13dff79
Saejin <saejinmx@gmail.com>
2024-04-30 15:33:30 +0200

    Réduction code

--- index.php
+++ index.php
@@ -4,18 +4,15 @@
     require "./config.php";
     global $dbAstra;
 
-
     if (isset($_POST['create'])) {
         require "./models/form.php";
     } elseif (isset($_POST['login'])) {
         require "./models/users.php";
     }
 
-
     //Affichage des pages
     require './template/header.php';
 
-    
     // var_dump($_GET['url']);
     // var_dump("./views/{$_GET['url']}.php");
 
@@ -26,8 +23,5 @@
     else {
         require "./views/{$_GET['url']}.php";
     }
-    
-    
-    
 
     require './template/footer.html';
