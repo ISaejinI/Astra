e943f8e08e8ad6f83a748eccf16e31861b03f39a
Saejin <saejinmx@gmail.com>
2024-05-02 13:51:50 +0200

    Affichage de toutes les informations

--- views/planet.php
+++ views/planet.php
@@ -1,11 +1,30 @@
 <?php 
 
 $req = 'SELECT * FROM planets WHERE id=?';
+$reqenv = 'SELECT name FROM environnements JOIN `planet-environnement` ON idEnvironnement=environnements.id WHERE idPlanet=?';
+$reqpop = 'SELECT name FROM populations JOIN `planet-population` ON idPopulation=populations.id WHERE idPlanet=?';
 
 $planet = $dbAstra->prepare($req);
 $planet->execute(array($_GET['id']));
-$line = $planet->fetch();
+$laplanete = $planet->fetch();
 
-var_dump($line);
+echo '<pre>';
+var_dump($laplanete);
+echo '</pre>';
 
+$env = $dbAstra->prepare($reqenv);
+$env->execute(array($_GET['id']));
+$envts = $env->fetchAll();
+
+echo '<pre>';
+var_dump($envts);
+echo '</pre>';
+
+$pop = $dbAstra->prepare($reqpop);
+$pop->execute(array($_GET['id']));
+$pops = $pop->fetchAll();
+
+echo '<pre>';
+var_dump($pops);
+echo '</pre>';
 ?>
