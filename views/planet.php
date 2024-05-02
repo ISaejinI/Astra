b52e4cc80adba3a914cc02619aec1dcad2d562e1
Saejin <saejinmx@gmail.com>
2024-05-02 15:54:27 +0200

    Début CSS planet

--- views/planet.php
+++ views/planet.php
@@ -1,5 +1,4 @@
 <?php
-
 $req = 'SELECT planets.*, galaxies.name AS galaxy_name FROM planets JOIN galaxies ON planets.idGalaxie=galaxies.id WHERE planets.id=?';
 
 $reqenv = 'SELECT name FROM environnements JOIN `planet-environnement` ON idEnvironnement=environnements.id WHERE idPlanet=?';
@@ -14,71 +13,74 @@ $env->execute(array($_GET['id']));
 $envts = $env->fetchAll();
 ?>
 
-<div id="desc">
-    <h4>Description</h4>
-    <p><?php echo $laplanete['description'] ?></p>
-</div>
+<section id="oneplanet">
+    <div id="desc">
+        <h4>Description</h4>
+        <p><?php echo $laplanete['description'] ?></p>
+    </div>
 
-<div id="visu">
-    <img src="/src/<?php echo $laplanete['urlImg'] ?>" alt="">
-    <h1 id="planetName"><?php echo $laplanete['name'] ?></h1>
-</div>
+    <div id="visu">
+        <img src="/src/<?php echo $laplanete['urlImg'] ?>" alt="">
+        <h1 id="planetName"><?php echo $laplanete['name'] ?></h1>
+    </div>
 
-<div id="infos">
-    <h4>Galaxie</h4>
-    <p><?php echo $laplanete['galaxy_name'] ?></p>
+    <div id="infos">
+        <h4>Galaxie</h4>
+        <p><?php echo $laplanete['galaxy_name'] ?></p>
 
-    <h4>Taille</h4>
-    <p><?php echo $laplanete['taille'] ?> km de diamètre</p>
+        <h4>Taille</h4>
+        <p><?php echo $laplanete['taille'] ?> km de diamètre</p>
 
-    <h4>Climat</h4>
-    <p><?php echo $laplanete['climat'] ?></p>
+        <h4>Climat</h4>
+        <p><?php echo $laplanete['climat'] ?></p>
 
-    <h4>Environnement</h4>
-    <ul>
+        <h4>Environnement</h4>
+        <ul>
+            <?php
+            foreach ($envts as $e) {
+                echo "<li>" . $e['name'] . "</li>";
+            }
+            ?>
+        </ul>
+
         <?php
-        foreach ($envts as $e) {
-            echo "<li>" . $e['name'] . "</li>";
+        if (is_null($laplanete['habitable'])) {
+            echo "<h4>Population</h4>";
+            echo "<p>La planète n'est aps habitable</p>";
+        } else {
+            echo "<h4>Nopmbre d'habitants</h4>";
+            echo "<p>" . $laplanete['nbHab'] . "</p>";
+
+            $pop = $dbAstra->prepare($reqpop);
+            $pop->execute(array($_GET['id']));
+            $pops = $pop->fetchAll();
+
+            echo "<h4>Population</h4>";
+            echo "<ul>";
+            foreach ($pops as $p) {
+                echo "<li>" . $p['name'] . "</li>";
+            }
+            echo "</ul>";
         }
         ?>
-    </ul>
 
+    </div>
+
+
+</section>
     <?php
-    if (is_null($laplanete['habitable'])) {
-        echo "<h4>Population</h4>";
-        echo "<p>La planète n'est aps habitable</p>";
+    if (is_null($laplanete['idUser'])) {
+        echo "";
     } else {
-        echo "<h4>Nopmbre d'habitants</h4>";
-        echo "<p>".$laplanete['nbHab']."</p>";
+        $requsr = 'SELECT id, username FROM users WHERE id=?';
+        $usr = $dbAstra->prepare($requsr);
+        $usr->execute(array($laplanete['idUser']));
+        $usrinfo = $usr->fetch();
 
-        $pop = $dbAstra->prepare($reqpop);
-        $pop->execute(array($_GET['id']));
-        $pops = $pop->fetchAll();
+        echo "<p>Planète créée par " . $usrinfo['username'] . "</p>";
 
-        echo "<h4>Population</h4>";
-        echo "<ul>";
-        foreach ($pops as $p) {
-            echo "<li>" . $p['name'] . "</li>";
-        }
-        echo "</ul>";
+        // echo "<pre>";
+        // var_dump($usrinfo);
+        // echo "</pre>"; 
     }
     ?>
-
-</div>
-
-<?php
-if (is_null($laplanete['idUser'])) {
-    echo "";
-} else {
-    $requsr = 'SELECT id, username FROM users WHERE id=?';
-    $usr = $dbAstra->prepare($requsr);
-    $usr->execute(array($laplanete['idUser']));
-    $usrinfo = $usr->fetch();
-
-    echo "<p>Planète créée par " . $usrinfo['username'] . "</p>";
-
-    // echo "<pre>";
-    // var_dump($usrinfo);
-    // echo "</pre>"; 
-}
-?>
