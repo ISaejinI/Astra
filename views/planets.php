78eef4dc3f62570e1d9e0b68a0ab0e398b6c7a19
Saejin <saejinmx@gmail.com>
2024-05-15 13:26:53 +0200

    Affichage des filtres avec accordéon

--- views/planets.php
+++ views/planets.php
@@ -1,10 +1,24 @@
 <section id="all-planets">
     <div id="filter">
         <h2>Filtres</h2>
-        <h3>Galaxies <i class='bx bx-chevron-down'></i></h3>
-        <h3>Environnements <i class='bx bx-chevron-down'></i></h3>
-        <h3>Populations <i class='bx bx-chevron-down'></i></h3>
+        <button class="accordion">Galaxies</button>
+        <div class="panel">
+            <p>Contenu de ma div</p>
+        </div>
+
+        <button class="accordion">Environnements</button>
+        <div class="panel">
+            <p>Contenu de ma div</p>
+        </div>
+
+        <button class="accordion">Populations</button>
+        <div class="panel">
+            <p>Contenu de ma div</p>
+        </div>
     </div>
+
+    <script src="script/script.js"></script>
+
     <div id="planets">
         <?php
             $req = 'SELECT * FROM planets ORDER BY id DESC';
